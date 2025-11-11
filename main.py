184b2ab316f69aa8cd1934869335a3a1f0d76250
Bailey Stringer <bailey4770@outlook.com>
2025-11-11 22:02:54 +0900

    added stats as separate module. counts number of words in a text

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("greetings boots")
+from stats import get_num_words
+
+
+def get_book_text(filepath: str):
+    with open(filepath, "r") as file:
+        content = file.read()
+    return content
+
+
+def main():
+    frank_path = "books/frankenstein.txt"
+    frankenstein_text = get_book_text(frank_path)
+    num_words = get_num_words(frankenstein_text)  # Print the first 500 characters
+    print(f"Found {num_words} total words")
+
+
+if __name__ == "__main__":
+    main()
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_num_words(text: str) -> int:
+    text_list = text.split()
+    return len(text_list)
